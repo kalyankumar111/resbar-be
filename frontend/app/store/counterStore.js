7c1e71ad0e8d47cc54053e6e59fe9beeac21e88c
kalyankumar111 <kumarkalyan736@gmail.com>
2025-12-25 17:25:03 +0530

    chore: delete frontend Nuxt application.

--- frontend/app/store/counterStore.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'zustand/vanilla'
-
-export const counterStore = createStore((set) => ({
-    count: 0,
-    inc: () => set((state) => ({ count: state.count + 1 })),
-    dec: () => set((state) => ({ count: state.count - 1 })),
-    reset: () => set({ count: 0 }),
-}))
